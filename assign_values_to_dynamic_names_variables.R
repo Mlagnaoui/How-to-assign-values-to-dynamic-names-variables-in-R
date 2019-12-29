0f8c8ce14b218d0448075d4652533c1672f5ec4c
Mlagnaoui <majid.lagnaoui@yahoo.fr>
2019-12-29 14:37:58 +0100

    Update assign_values_to_dynamic_names_variables.R

--- assign_values_to_dynamic_names_variables.R
+++ assign_values_to_dynamic_names_variables.R
@@ -1,6 +1,5 @@
 name <- c("Baptiste","Nicolas","Maxime")
 
-
 for (word in name){
-  eval(parse(text=paste("grade_",word," <- sample(0:20,1)",sep="")))
+  eval(parse(text=paste("score_",word," <- sample(0:20,1)",sep="")))
 }
